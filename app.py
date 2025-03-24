f804c502d122722577258d2c82d0f338673e35fe
Laurence Castillo <laurence.castillo@me.com>
2025-03-24 02:25:35 -0400

    login backend written, but untested

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 import sqlite3 as sql
 #from hashlib import sha256
 import bcrypt
@@ -16,22 +16,27 @@ def index():
 
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
-    email = request.form['email']
+    if request.method == 'POST':
+        email = request.form['email']
 
-    password = request.form['password']
-    salt = bcrypt.gensalt() #will strengthen the generated hash value
-    hashed_password = bcrypt.hashpw(password.encode('utf-8',salt))
+        password = request.form['password']
+        salt = bcrypt.gensalt() #will strengthen the generated hash value
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'),salt)
 
-    if check_email(email):
-        if check_password(email,hashed_password):
-            #navigate to next page
-            pass
+        if check_email(email):
+            if check_password(email,hashed_password):
+                #navigate to next page
+                return redirect(url_for('home')) #insert name of page here
+            else:
+                #notify that password is incorrect
+                error = 'Incorrect password. Please try again.'
+                return render_template('login.html', error = error)
+            
         else:
-            #notify that password is incorrect
-            pass
-    else:
-        #notify that email is incorrect
-        pass
+            #notify that email is incorrect
+            error = 'Email not found. Please try again.'
+            return render_template('login.html', error = error)
+    
 
 def check_email(email):
     connection = sql.connect('database.db')
@@ -41,7 +46,7 @@ def check_email(email):
     result = cursor.fetchone()
     connection.close()
 
-    if result and result[0] > 0: #check if cursor retrieved a row
+    if result: #check if cursor retrieved a row
         return True
     return False
 
@@ -53,7 +58,7 @@ def check_password(email, hashed_password):
     result = cursor.fetchone()
     connection.close()
 
-    if result:
+    if bcrypt.checkpw(hashed_password,result[0]):
         return True
     return False
 
