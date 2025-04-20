81685dab8a95231f583121d6f0da491ef53748a0
Laurence Castillo <laurence.castillo@me.com>
2025-04-20 00:20:36 -0400

    Debugged load_data.py and ran it to add all data to database.db

--- load_data.py
+++ load_data.py
@@ -18,7 +18,7 @@ with sql.connect('database.db') as connection:
 
             table = path[:-4] #remove .csv extension 
             for row in reader:
-                if table == 'Address': #remember to clear tables if there is an error in running this file
+                if table == 'Address': #remember to clear tables if there is an error in running this file DELETE * FROM table_name
                     cursor.execute('INSERT INTO Address VALUES (?, ?, ?, ?)', row) #put OR IGNORE after done testing.
                     
                 elif table == 'Buyers':
@@ -40,20 +40,23 @@ with sql.connect('database.db') as connection:
                     
                 elif table == 'Product_Listings': #TO DO: Remove '$' from Product_Price column
 
-                    row[8] = int(row[8].replace('$','')) #SCHEMA HAS PRICE AS VARCHAR: WILL RAISE ERROR. CONSIDER CHANGING SCHEMA OR OMITT THIS LINE
+                    #row[8] = int(row[8].replace('$','')) #SCHEMA HAS PRICE AS VARCHAR: WILL RAISE ERROR. CONSIDER CHANGING SCHEMA OR OMITT THIS LINE
+                    row[8] = row[8].replace('$','') #remember to turn to int if doing any operations with product price
                     cursor.execute('INSERT INTO Products VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', row)
                     
                 elif table == 'Requests':
-                    cursor.execute('INSERT INTO Requests VALUES (?, ?, ?)', row)
+                    cursor.execute('INSERT INTO Requests VALUES (?, ?, ?, ?, ?, ?)', row)
 
                 elif table == 'Reviews':
                     cursor.execute('INSERT INTO Reviews VALUES (?, ?, ?)', row)
                     
                 elif table == 'Sellers': #DISCUSS IF WE WANT TO KEEP ROUTING NUMBER AS A VARCHAR OR CHANGE TO INT
-                    cursor.execute('INSERT INTO Seller VALUES (?, ?, ?, ?, ?, ?)')
+                    cursor.execute('INSERT INTO Seller VALUES (?, ?, ?, ?, ?, ?)', row)
 
                 #elif table == 'Users': #Users were already hashed and inserted during the progress check, make sure to hash if using this line again
                 #    cursor.execute('INSERT OR IGNORE INTO Users VALUES (?, ?)', row)
 
                 elif table == 'Zipcode_Info':
                     cursor.execute('INSERT INTO Zipcode VALUES (?, ?, ?)', row)
+        
+connection.commit()
